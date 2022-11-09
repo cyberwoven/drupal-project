e05026dca8520d4a9b242d6533b3d895fa52d232
Drew King <drew@drewcking.com>
2022-11-09 07:34:15 -0500

    test out post-create-project-cmd

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo "Installing..."
+echo " - Enable modules"
+echo " - Disable modules"
+echo " - Import partial configs (defaults for ckeditor)"
+echo " - Disable modules"
+echo " - Disable modules"
+echo " - Disable modules"
+
